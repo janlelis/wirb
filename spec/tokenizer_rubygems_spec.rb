707300c2ed1459ae80bd84ad9ccf201c4c77c27c
Jan Lelis <mail@janlelis.de>
2011-06-15 02:03:15 +0200

    only test real gem stuff if using a new gem version (for travis)

--- spec/tokenizer_rubygems_spec.rb
+++ spec/tokenizer_rubygems_spec.rb
@@ -25,25 +25,6 @@ describe tokenizer(__FILE__) do
     ]
   end
 
-  only19 do
-    please do
-      require 'rspec'
-      check [Gem::Specification.find_by_name('rspec').dependencies.first.requirement]*2
-      tokens.should == [
-        [:open_array, '['],
-        [:gem_requirement_condition, "~>"],
-        [:whitespace, " "],
-        [:gem_requirement_version, RSpec::Version::STRING],
-        [:comma, ","],
-        [:whitespace, " "],
-        [:gem_requirement_condition, "~>"],
-        [:whitespace, " "],
-        [:gem_requirement_version, RSpec::Version::STRING],
-        [:close_array, ']'],
-      ]
-    end
-  end
-
   please do check_inspected "{1=>>= 0}"
     tokens.should == [
       [:open_hash, '{'],
@@ -56,24 +37,44 @@ describe tokenizer(__FILE__) do
     ]
   end
 
-  please do
-    require 'rspec'
-    check Gem::Specification.find_by_name('wirb').dependencies.first
-    tokens.should == [ # <Gem::Dependency type=:development name="rspec" requirements=">= 0">
-      [:open_object, "<"],
-      [:object_class, "Gem"],
-      [:class_separator, "::"],
-      [:object_class, "Dependency"],
-      [:object_description_prefix, " "],
-      [:object_description, "type=:development name="],
-      [:open_string, "\""],
-      [:string, "rspec"],
-      [:close_string, "\""],
-      [:object_description, " requirements="],
-      [:open_string, "\""],
-      [:string, ">= 0"],
-      [:close_string, "\""],
-      [:close_object, ">"],
-    ]
+  if Gem::Specification.respond_to? :find_by_name
+    only19 do
+      please do
+        require 'rspec'
+        check [Gem::Specification.find_by_name('rspec').dependencies.first.requirement]*2
+        tokens.should == [
+          [:open_array, '['],
+          [:gem_requirement_condition, "~>"],
+          [:whitespace, " "],
+          [:gem_requirement_version, RSpec::Version::STRING],
+          [:comma, ","],
+          [:whitespace, " "],
+          [:gem_requirement_condition, "~>"],
+          [:whitespace, " "],
+          [:gem_requirement_version, RSpec::Version::STRING],
+          [:close_array, ']'],
+        ]
+      end
+    end
+
+    please do
+      check Gem::Specification.find_by_name('wirb').dependencies.first
+      tokens.should == [ # <Gem::Dependency type=:development name="rspec" requirements=">= 0">
+        [:open_object, "<"],
+        [:object_class, "Gem"],
+        [:class_separator, "::"],
+        [:object_class, "Dependency"],
+        [:object_description_prefix, " "],
+        [:object_description, "type=:development name="],
+        [:open_string, "\""],
+        [:string, "rspec"],
+        [:close_string, "\""],
+        [:object_description, " requirements="],
+        [:open_string, "\""],
+        [:string, ">= 0"],
+        [:close_string, "\""],
+        [:close_object, ">"],
+      ]
+    end
   end
 end
