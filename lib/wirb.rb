227c73ac48ad01ab5e834cf4fe56aee67e538f3d
Jan Lelis <mail@janlelis.de>
2015-01-25 17:21:11 +0100

    remove colorization abstraction, always use paint

--- lib/wirb.rb
+++ lib/wirb.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + '/wirb/version'
-require File.dirname(__FILE__) + '/wirb/colorizer'
 require File.dirname(__FILE__) + '/wirb/tokenizer'
+
 require 'yaml'
+require 'paint'
 
 class << Wirb
   def running?() @running end
@@ -25,24 +26,6 @@ class << Wirb
   alias deactivate stop
   alias disable    stop
 
-  # extend getter & setter for colorizer & schema
-  def colorizer
-    @colorizer ||= Wirb::Colorizer::Paint
-  end
-
-  def colorizer=(col)
-    @colorizer = col
-  end
-
-  # Convenience method, permits simplified syntax like:
-  #   Wirb.load_colorizer :HighLine
-  def load_colorizer(colorizer_name)
-    # @colorizer = Wirb::Colorizer.const_get(colorizer_name, false)
-    @colorizer = eval "Wirb::Colorizer::#{colorizer_name}"
-    compatible_colorizer?(@colorizer)
-    @colorizer
-  end
-
   def schema
     @schema || load_schema
   end
@@ -54,7 +37,7 @@ class << Wirb
   # Loads a color schema from a yaml file
   #   If first argument is a String: path to yaml file
   #   If first argument is a Symbol: bundled schema
-  def load_schema!(yaml_path = :classic_paint)
+  def load_schema!(yaml_path = :classic)
     if yaml_path.is_a? Symbol # bundled themes
       schema_name = yaml_path.to_s
       schema_yaml = YAML.load_file(File.join(Gem.datadir('wirb'), schema_name + '.yml'))
@@ -68,7 +51,6 @@ class << Wirb
         [k.to_s.to_sym, Array( v )]
       } ]
       @schema[:name] = schema_name.to_sym
-      @schema[:colorizer].map!(&:to_sym)
     else
       raise LoadError, "Could not load the Wirb schema at: #{yaml_path}"
     end
@@ -76,22 +58,21 @@ class << Wirb
     @schema
   end
 
-  # Loads a color schema from a yaml file and sets colorizer to first suggested one in schema
-  def load_schema(yaml_path = :classic_paint)
+  # Loads a color schema from a yaml file
+  def load_schema(yaml_path = :classic)
     load_schema! yaml_path
-    load_colorizer schema[:colorizer].first
     @schema
   end
 
   # Return the escape code for a given color
   def get_color(*keys)
-    colorizer.color(*keys)
+    Paint.color(*keys)
   end
   alias color get_color
-  
+
   # Colorize a string
   def colorize_string(string, *colors)
-    colorizer.run(string, *colors)
+    Paint[string, *colors]
   end
 
   # Colorize a result string
@@ -112,24 +93,7 @@ class << Wirb
   alias colorize_code colorize_result
   alias colorize_ruby colorize_result
 
-  private
-
-  def compatible_colorizer?(col)
-    short_col = col.to_s.sub(/^.*::(.*?)$/, '\1').to_sym
-    if !col
-      warn "No colorizer selected!"
-      false
-    elsif !Array( @schema[:colorizer] ).include?(short_col)
-      warn "Your current color schema does not support this colorizer:\n" +
-           "  #{ short_col }\n" + 
-           "The following colorizeres are supported with this schema (use Wirb.load_colorizer :Name):\n  " + 
-           Array( @schema[:colorizer] ).join("\n  ")
-      false
-    else
-      true
-    end
-  end
-
 end
 
 # J-_-L
+#
--- spec/paint_integration_spec.rb
+++ spec/paint_integration_spec.rb
@@ -1,11 +1,4 @@
-begin # only test if paint is available
-require 'paint'
-
-describe Wirb::Colorizer::Wirb0_Paint do
-  before :each do
-    Wirb.colorizer = Wirb::Colorizer::Wirb0_Paint
-  end
-  
+describe 'paint integration' do
   it "creates good color codes based on Paint color symbols" do
     Wirb.get_color(:red).should match_colors(31)
     Wirb.get_color(:red, :bright).should match_colors(1,31)
@@ -19,17 +12,9 @@ describe Wirb::Colorizer::Wirb0_Paint do
     Wirb.get_color("#863").should match_colors('38;5;' + (16 + 3*36 + 2*6 + 1*1).to_s)
     Wirb.get_color(:red, :encircle).should match_colors(52,31)
   end
-  
+
   it "colorizes strings based on Paint color symbols" do
     Wirb.colorize_string('Dantooine', :red, :bright, :underline).should match_colored_string('Dantooine', 1,4,31)
   end
-  
-  it_behaves_like "a Wirb0 colorizer"
-  
-  after :each do
-    Wirb.colorizer = nil
-  end
 end
 
-rescue LoadError
-end
