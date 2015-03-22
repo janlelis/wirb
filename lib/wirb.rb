079f72734ea923c3c2182e231439fde3537a37fc
Jan Lelis <mail@janlelis.de>
2015-03-22 14:25:42 +0100

    open singleton class more explicitely

--- lib/wirb.rb
+++ lib/wirb.rb
@@ -9,84 +9,82 @@ require 'paint'
 require 'timeout'
 
 
-class << Wirb
-  def running?
-    defined?(IRB) && IRB.CurrentContext &&
-    IRB.CurrentContext.inspect_mode == :wirb
-  end
-
-  def timeout
-    @timeout ||= 3
-  end
-
-  def schema
-    @schema ||= Wirb::SchemaBuilder.load_schema_from_yaml
-  end
-
-  def schema=(val)
-    @schema = val
-  end
-
-  def load_schema(name)
-    @schema = Wirb::SchemaBuilder.load_schema_from_yaml(name)
-  end
-
-  def original_inspect_mode
-    @original_inspect_mode
-  end
-
-  def start
-    require_relative 'wirb/irb'
-    @original_inspect_mode =
-      IRB.conf[:INSPECT_MODE] ||
-      IRB.CurrentContext && IRB.CurrentContext.inspect_mode ||
-      true
-    IRB.conf[:INSPECT_MODE] = :wirb
-    IRB.CurrentContext.inspect_mode = :wirb if IRB.CurrentContext
-    true
-  end
-  alias activate start
-  alias enable   start
-
-  def stop
-    if running?
-      IRB.conf[:INSPECT_MODE] = @original_inspect_mode
-      IRB.CurrentContext.inspect_mode = @original_inspect_mode if IRB.CurrentContext
-      true
-    end
-  end
-  alias deactivate stop
-  alias disable    stop
-
-  def colorize_result(string, _deprecated = nil)
-    check = ''
-    colorful = ''
-    begin
-      Wirb::Tokenizer.run(string).each{ |kind, token|
-        check << token
-        colorful << Paint[token, *Array( schema[kind] )]
-      }
-    rescue
-      p $!, $!.backtrace[0] if $VERBOSE
+module Wirb
+  class << self
+    def running?
+      defined?(IRB) && IRB.CurrentContext &&
+      IRB.CurrentContext.inspect_mode == :wirb
     end
 
-    # always display the correct inspect string!
-    check == string ? colorful : string
-  end
+    def timeout
+      @timeout ||= 3
+    end
 
-  def colorize_result_with_timeout(string)
-    if timeout.to_i == 0
-      colorize_result(string)
-    else
-      Timeout.timeout(timeout) do
-        colorize_result(string)
+    def schema
+      @schema ||= SchemaBuilder.load_schema_from_yaml
+    end
+
+    def schema=(val)
+      @schema = val
+    end
+
+    def load_schema(name)
+      @schema = SchemaBuilder.load_schema_from_yaml(name)
+    end
+
+    def original_inspect_mode
+      @original_inspect_mode
+    end
+
+    def start
+      require_relative 'wirb/irb'
+      @original_inspect_mode =
+        IRB.conf[:INSPECT_MODE] ||
+        IRB.CurrentContext && IRB.CurrentContext.inspect_mode ||
+        true
+      IRB.conf[:INSPECT_MODE] = :wirb
+      IRB.CurrentContext.inspect_mode = :wirb if IRB.CurrentContext
+      true
+    end
+    alias activate start
+    alias enable   start
+
+    def stop
+      if running?
+        IRB.conf[:INSPECT_MODE] = @original_inspect_mode
+        IRB.CurrentContext.inspect_mode = @original_inspect_mode if IRB.CurrentContext
+        true
       end
     end
-  rescue Timeout::Error
-    string
+    alias deactivate stop
+    alias disable    stop
+
+    def colorize_result(string, _deprecated = nil)
+      check = ''
+      colorful = ''
+      begin
+        Tokenizer.run(string).each{ |kind, token|
+          check << token
+          colorful << Paint[token, *Array( schema[kind] )]
+        }
+      rescue
+        p $!, $!.backtrace[0] if $VERBOSE
+      end
+
+      # always display the correct inspect string!
+      check == string ? colorful : string
+    end
+
+    def colorize_result_with_timeout(string)
+      if timeout.to_i == 0
+        colorize_result(string)
+      else
+        Timeout.timeout(timeout) do
+          colorize_result(string)
+        end
+      end
+    rescue Timeout::Error
+      string
+    end
   end
-
 end
-
-# J-_-L
-
